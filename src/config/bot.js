4ec3ec1e9cd2b8ea0a94d06a25b3c3b170054fe7
Zaepex <kahlirodriguez1@gmail.com>
2026-06-19 18:28:51 -0600

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Use my commands if you need help",
         // Activity type number (0 = Playing).
         type: 0,
       },
